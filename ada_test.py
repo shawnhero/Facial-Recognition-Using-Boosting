c1c9742f8a684018131e7b5d8c9c6471298611f6
shawnhero <shawn.xuwu@gmail.com>
2014-11-19 13:15:01 -0800

    changes

--- ada_test.py
+++ ada_test.py
@@ -105,11 +105,11 @@ class ProcessWorker(Process):
 				print "fType"+str(self.ftype)+", mID"+str(mid)+", {0:.0%}".format(1.0*i/len(rowlist)), "done.."
 				lock.release()
 			error_infor = self.FindFeatureError(row)
-			print "error", error_infor[1]
 			if error_infor[1]<minError:
 				minError = error_infor[1]
 				minResult = error_infor
 				minRow = row
+		print "map min error", minResult[1]
 		self.mapResult.append((minResult,minRow))
 		print "fType"+str(self.ftype)+", mID"+str(mid)+" finished."
 
